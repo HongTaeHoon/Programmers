d85ec52c403d80c1cae076086912602cfc9d1049
HongTaeHoon <122140479+HongTaeHoon@users.noreply.github.com>
2023-10-19 15:02:36 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.69 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,7 @@
+function solution(arr, intervals) {
+const [[a,b],[c,d]] = intervals
+    return [...arr.slice(a,b+1),...arr.slice(c,d+1)]
+  //   arr1= arr.slice(intervals[0][0],intervals[0][1]+1);
+  //   arr2=arr.slice(intervals[1][0],intervals[1][1]+1);
+  // return arr1.concat(arr2)
+}
