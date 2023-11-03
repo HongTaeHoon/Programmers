a08e034758f112337445534952c0ab91081e675e
HongTaeHoon <122140479+HongTaeHoon@users.noreply.github.com>
2023-11-03 16:16:52 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,15 @@
+function solution(ingredient) {
+let burger = 0
+let pack = []
+for(i = 0 ; i < ingredient.length+1 ; i++){
+    if(pack.slice(-4).join('') === '1231'){
+        pack.splice(pack.length-4,4)
+        burger += 1
+        pack.push(ingredient[i])
+    }else{
+        pack.push(ingredient[i])
+    }
+}
+    return burger
+
+}
