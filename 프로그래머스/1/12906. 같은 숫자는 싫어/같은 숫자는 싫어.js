36a004f404d04622595db0f551c8854906d77eb3
HongTaeHoon <122140479+HongTaeHoon@users.noreply.github.com>
2023-11-05 15:03:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 42.70 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,8 +1,9 @@
 function solution(arr)
 {
-const answer = [];
-    for(i=0; i<arr.length; i++){
-           if(arr[i] !== arr[i+1])answer.push(arr[i])
-    }
-return answer
+// const answer = [];
+//     for(i=0; i<arr.length; i++){
+//            if(arr[i] !== arr[i+1])answer.push(arr[i])
+//     }
+// return answer
+    return arr.filter((a,idx)=> a !==arr[idx+1])
 }
