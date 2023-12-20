41eff39372b398e655e83148e606d61431b5eb14
HongTaeHoon <122140479+HongTaeHoon@users.noreply.github.com>
2023-12-20 22:05:53 +0900

    [level unrated] Title: 카운트 업, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,10 @@
+function solution(start_num, end_num) {
+    let answer = [];
+    let n  = start_num;
+    while( n <= end_num){
+        answer.push(n)
+        n++
+        
+    }
+ return answer
+}
